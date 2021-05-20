af7a44532b734ab9c2c44f04894b7feb92ef7a64
Ws-Peroth <a01033056809@gmail.com>
2021-05-20 18:49:18 +0900

    3D Test
    3D Test

---
+++ 3D_ShootingGame/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private void Start()
+    {
+        speed = 5f;
+    }
+
+    public void Walk()
+    {
+        // if (Input.GetKey(KeyCode.LeftArrow)) { transform.Translate(-speed * Time.deltaTime, 0, 0); }
+        // if (Input.GetKey(KeyCode.RightArrow)) { transform.Translate(speed * Time.deltaTime, 0, 0); }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+    }
+
+    public void Spin()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -0.5f, 0);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0.5f, 0);
+        }
+    }
+
+    void Update()
+    {
+        Walk();
+        Spin();
+    }
+}
