c89d69e7f9792436625dc841d5459bd44405dfeb
Ws-Peroth <a01033056809@gmail.com>
2021-05-28 23:31:59 +0900

    Enemy Pattern Call
    Make Enemy Pattern Calling Frame

---
+++ 3D_ShootingGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool isPatternOn;
+    public delegate void pattern();
+    public List<pattern> patterns;
+
+    private void Start()
+    {
+        isPatternOn = false;
+        patterns = new List<pattern>
+        {
+            Pattern01
+        };
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (!isPatternOn)
+            {
+                CallPattern();
+            }
+        }
+    }
+
+    public void CallPattern()
+    {
+        int selectPattern = Random.Range(0, patterns.Count);
+        patterns[selectPattern]();
+    }
+
+    public void Pattern01()
+    {
+        isPatternOn = true;
+        print("call pattern 01");
+    }
+}
